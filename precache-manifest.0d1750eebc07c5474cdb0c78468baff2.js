187ddf4f1472e05ada288864024e3cc31715a7d0
Quý Phạm <yungbloodx02@gmail.com>
2020-09-20 23:56:45 +0700

    new blog

---
+++ precache-manifest.0d1750eebc07c5474cdb0c78468baff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86244c6b9461e32b3645de5f6ebb1a55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58b450eeb52ac9713991",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "72410df7cd63c34a8e96",
+    "url": "/static/css/main.55f86305.chunk.css"
+  },
+  {
+    "revision": "58b450eeb52ac9713991",
+    "url": "/static/js/2.ccbdc29c.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.ccbdc29c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72410df7cd63c34a8e96",
+    "url": "/static/js/main.61b0637b.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
